0890c4b4b10bde11d00919b014edf2b3d94b561b
Andreina Sananez <asananez2000@gmail.com>
2024-04-29 08:42:13 -0600

    Add comparison operators and simple IF statement

--- compiler/parsetab.py
+++ compiler/parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'COMMA CONNECT DIVIDE EXP LPAREN MINUS NUMBER PLUS RPAREN SETTO STRING TIMES VARIABLE\n    assignment : VARIABLE SETTO expression\n    \n    assignment : VARIABLE SETTO flow\n    \n    flow : VARIABLE CONNECT flow_functions\n    \n    flow_functions : flow_function_call CONNECT flow_functions\n    \n    flow_functions : flow_function_call\n    \n    flow_function_call : VARIABLE LPAREN params RPAREN\n     assignment : expression\n    \n    expression : expression PLUS term\n    \n    expression : expression MINUS term\n    \n    expression : term \n                | string\n    \n    string : STRING\n    \n    term : term TIMES exponent\n    \n    term : term DIVIDE exponent\n    \n    term : exponent\n    \n    exponent : factor EXP factor\n    \n    exponent : factor\n    \n    exponent : LPAREN expression RPAREN\n     factor : NUMBER\n     factor : VARIABLE\n    \n    factor : function_call\n    \n    function_call : VARIABLE LPAREN  RPAREN\n    \n    function_call : VARIABLE LPAREN params RPAREN\n    \n    params : params COMMA expression \n            | expression\n    '
+_lr_signature = 'COLON COMMA CONNECT DIVIDE EQ EXP GE GT IF LE LPAREN LT MINUS NE NUMBER PLUS RPAREN SETTO STRING TIMES VARIABLE\n    assignment : VARIABLE SETTO expression\n    \n    assignment : VARIABLE SETTO flow\n    \n    flow : VARIABLE CONNECT flow_functions\n    \n    flow_functions : flow_function_call CONNECT flow_functions\n    \n    flow_functions : flow_function_call\n    \n    flow_function_call : VARIABLE LPAREN params RPAREN\n     assignment : expression\n    \n    expression : expression PLUS term\n    \n    expression : expression MINUS term\n    \n    expression : term \n                | string\n    \n    string : STRING\n    \n    term : term TIMES exponent\n    \n    term : term DIVIDE exponent\n    \n    term : exponent\n    \n    exponent : factor EXP factor\n    \n    exponent : factor\n    \n    exponent : LPAREN expression RPAREN\n     factor : NUMBER\n     factor : VARIABLE\n    \n    expression : expression GT expression\n    \n    expression : expression LT expression\n    \n    expression : expression GE expression\n    \n    expression : expression LE expression\n    \n    expression : expression EQ expression\n    \n    expression : expression NE expression\n    \n    factor : function_call\n    \n    function_call : VARIABLE LPAREN  RPAREN\n    \n    function_call : VARIABLE LPAREN params RPAREN\n    \n    params : params COMMA expression \n            | expression\n    expression : IF LPAREN expression RPAREN COLON expression'
     
-_lr_action_items = {'VARIABLE':([0,9,12,13,14,15,16,17,18,33,35,40,41,],[2,20,21,20,20,20,20,20,20,36,20,20,36,]),'STRING':([0,9,12,13,35,40,],[7,7,7,7,7,7,]),'LPAREN':([0,2,9,12,13,14,15,16,17,20,21,35,36,40,],[9,13,9,9,9,9,9,9,9,13,13,9,40,9,]),'NUMBER':([0,9,12,13,14,15,16,17,18,35,40,],[10,10,10,10,10,10,10,10,10,10,10,]),'$end':([1,2,3,4,5,6,7,8,10,11,20,21,22,23,24,27,28,29,30,31,32,34,37,38,43,44,],[0,-20,-7,-10,-11,-15,-12,-17,-19,-21,-20,-20,-1,-2,-22,-8,-9,-13,-14,-16,-18,-23,-3,-5,-4,-6,]),'SETTO':([2,],[12,]),'EXP':([2,8,10,11,20,21,24,34,],[-20,18,-19,-21,-20,-20,-22,-23,]),'TIMES':([2,4,6,8,10,11,20,21,24,27,28,29,30,31,32,34,],[-20,16,-15,-17,-19,-21,-20,-20,-22,16,16,-13,-14,-16,-18,-23,]),'DIVIDE':([2,4,6,8,10,11,20,21,24,27,28,29,30,31,32,34,],[-20,17,-15,-17,-19,-21,-20,-20,-22,17,17,-13,-14,-16,-18,-23,]),'PLUS':([2,3,4,5,6,7,8,10,11,19,20,21,22,24,26,27,28,29,30,31,32,34,39,],[-20,14,-10,-11,-15,-12,-17,-19,-21,14,-20,-20,14,-22,14,-8,-9,-13,-14,-16,-18,-23,14,]),'MINUS':([2,3,4,5,6,7,8,10,11,19,20,21,22,24,26,27,28,29,30,31,32,34,39,],[-20,15,-10,-11,-15,-12,-17,-19,-21,15,-20,-20,15,-22,15,-8,-9,-13,-14,-16,-18,-23,15,]),'RPAREN':([4,5,6,7,8,10,11,13,19,20,24,25,26,27,28,29,30,31,32,34,39,42,],[-10,-11,-15,-12,-17,-19,-21,24,32,-20,-22,34,-25,-8,-9,-13,-14,-16,-18,-23,-24,44,]),'COMMA':([4,5,6,7,8,10,11,20,24,25,26,27,28,29,30,31,32,34,39,42,],[-10,-11,-15,-12,-17,-19,-21,-20,-22,35,-25,-8,-9,-13,-14,-16,-18,-23,-24,35,]),'CONNECT':([21,38,44,],[33,41,-6,]),}
+_lr_action_items = {'VARIABLE':([0,7,13,14,15,16,17,18,19,20,21,22,23,24,25,28,48,50,56,57,58,],[2,27,29,27,27,27,27,27,27,27,27,27,27,27,27,27,52,27,27,27,52,]),'IF':([0,7,13,14,17,18,19,20,21,22,25,50,56,57,],[6,6,6,6,6,6,6,6,6,6,6,6,6,6,]),'STRING':([0,7,13,14,17,18,19,20,21,22,25,50,56,57,],[9,9,9,9,9,9,9,9,9,9,9,9,9,9,]),'LPAREN':([0,2,6,7,13,14,15,16,17,18,19,20,21,22,23,24,25,27,29,50,52,56,57,],[7,14,25,7,7,7,7,7,7,7,7,7,7,7,7,7,7,14,14,7,57,7,7,]),'NUMBER':([0,7,13,14,15,16,17,18,19,20,21,22,23,24,25,28,50,56,57,],[11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,]),'$end':([1,2,3,4,5,8,9,10,11,12,27,29,30,31,32,35,36,37,38,39,40,41,42,43,44,46,47,49,53,54,59,61,62,],[0,-20,-7,-10,-11,-15,-12,-17,-19,-27,-20,-20,-1,-2,-28,-8,-9,-21,-22,-23,-24,-25,-26,-13,-14,-18,-16,-29,-3,-5,-32,-4,-6,]),'SETTO':([2,],[13,]),'EXP':([2,10,11,12,27,29,32,49,],[-20,28,-19,-27,-20,-20,-28,-29,]),'TIMES':([2,4,8,10,11,12,27,29,32,35,36,43,44,46,47,49,],[-20,23,-15,-17,-19,-27,-20,-20,-28,23,23,-13,-14,-18,-16,-29,]),'DIVIDE':([2,4,8,10,11,12,27,29,32,35,36,43,44,46,47,49,],[-20,24,-15,-17,-19,-27,-20,-20,-28,24,24,-13,-14,-18,-16,-29,]),'PLUS':([2,3,4,5,8,9,10,11,12,26,27,29,30,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,],[-20,15,-10,-11,-15,-12,-17,-19,-27,15,-20,-20,15,-28,15,-8,-9,15,15,15,15,15,15,-13,-14,15,-18,-16,-29,15,15,]),'MINUS':([2,3,4,5,8,9,10,11,12,26,27,29,30,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,],[-20,16,-10,-11,-15,-12,-17,-19,-27,16,-20,-20,16,-28,16,-8,-9,16,16,16,16,16,16,-13,-14,16,-18,-16,-29,16,16,]),'GT':([2,3,4,5,8,9,10,11,12,26,27,29,30,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,],[-20,17,-10,-11,-15,-12,-17,-19,-27,17,-20,-20,17,-28,17,-8,-9,17,17,17,17,17,17,-13,-14,17,-18,-16,-29,17,17,]),'LT':([2,3,4,5,8,9,10,11,12,26,27,29,30,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,],[-20,18,-10,-11,-15,-12,-17,-19,-27,18,-20,-20,18,-28,18,-8,-9,18,18,18,18,18,18,-13,-14,18,-18,-16,-29,18,18,]),'GE':([2,3,4,5,8,9,10,11,12,26,27,29,30,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,],[-20,19,-10,-11,-15,-12,-17,-19,-27,19,-20,-20,19,-28,19,-8,-9,19,19,19,19,19,19,-13,-14,19,-18,-16,-29,19,19,]),'LE':([2,3,4,5,8,9,10,11,12,26,27,29,30,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,],[-20,20,-10,-11,-15,-12,-17,-19,-27,20,-20,-20,20,-28,20,-8,-9,20,20,20,20,20,20,-13,-14,20,-18,-16,-29,20,20,]),'EQ':([2,3,4,5,8,9,10,11,12,26,27,29,30,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,],[-20,21,-10,-11,-15,-12,-17,-19,-27,21,-20,-20,21,-28,21,-8,-9,21,21,21,21,21,21,-13,-14,21,-18,-16,-29,21,21,]),'NE':([2,3,4,5,8,9,10,11,12,26,27,29,30,32,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,],[-20,22,-10,-11,-15,-12,-17,-19,-27,22,-20,-20,22,-28,22,-8,-9,22,22,22,22,22,22,-13,-14,22,-18,-16,-29,22,22,]),'RPAREN':([4,5,8,9,10,11,12,14,26,27,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,49,55,59,60,],[-10,-11,-15,-12,-17,-19,-27,32,46,-20,-28,49,-31,-8,-9,-21,-22,-23,-24,-25,-26,-13,-14,51,-18,-16,-29,-30,-32,62,]),'COMMA':([4,5,8,9,10,11,12,27,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,49,55,59,60,],[-10,-11,-15,-12,-17,-19,-27,-20,-28,50,-31,-8,-9,-21,-22,-23,-24,-25,-26,-13,-14,-18,-16,-29,-30,-32,50,]),'CONNECT':([29,54,62,],[48,58,-6,]),'COLON':([51,],[56,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'assignment':([0,],[1,]),'expression':([0,9,12,13,35,40,],[3,19,22,26,39,26,]),'term':([0,9,12,13,14,15,35,40,],[4,4,4,4,27,28,4,4,]),'string':([0,9,12,13,35,40,],[5,5,5,5,5,5,]),'exponent':([0,9,12,13,14,15,16,17,35,40,],[6,6,6,6,6,6,29,30,6,6,]),'factor':([0,9,12,13,14,15,16,17,18,35,40,],[8,8,8,8,8,8,8,8,31,8,8,]),'function_call':([0,9,12,13,14,15,16,17,18,35,40,],[11,11,11,11,11,11,11,11,11,11,11,]),'flow':([12,],[23,]),'params':([13,40,],[25,42,]),'flow_functions':([33,41,],[37,43,]),'flow_function_call':([33,41,],[38,38,]),}
+_lr_goto_items = {'assignment':([0,],[1,]),'expression':([0,7,13,14,17,18,19,20,21,22,25,50,56,57,],[3,26,30,34,37,38,39,40,41,42,45,55,59,34,]),'term':([0,7,13,14,15,16,17,18,19,20,21,22,25,50,56,57,],[4,4,4,4,35,36,4,4,4,4,4,4,4,4,4,4,]),'string':([0,7,13,14,17,18,19,20,21,22,25,50,56,57,],[5,5,5,5,5,5,5,5,5,5,5,5,5,5,]),'exponent':([0,7,13,14,15,16,17,18,19,20,21,22,23,24,25,50,56,57,],[8,8,8,8,8,8,8,8,8,8,8,8,43,44,8,8,8,8,]),'factor':([0,7,13,14,15,16,17,18,19,20,21,22,23,24,25,28,50,56,57,],[10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,47,10,10,10,]),'function_call':([0,7,13,14,15,16,17,18,19,20,21,22,23,24,25,28,50,56,57,],[12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,]),'flow':([13,],[31,]),'params':([14,57,],[33,60,]),'flow_functions':([48,58,],[53,61,]),'flow_function_call':([48,58,],[54,54,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,29 +27,36 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> assignment","S'",1,None,None,None),
-  ('assignment -> VARIABLE SETTO expression','assignment',3,'p_assignment_assign','translator.py',101),
-  ('assignment -> VARIABLE SETTO flow','assignment',3,'p_assignment_flow','translator.py',113),
-  ('flow -> VARIABLE CONNECT flow_functions','flow',3,'p_flow_form','translator.py',119),
-  ('flow_functions -> flow_function_call CONNECT flow_functions','flow_functions',3,'p_flow_functions','translator.py',125),
-  ('flow_functions -> flow_function_call','flow_functions',1,'p_flow_function','translator.py',131),
-  ('flow_function_call -> VARIABLE LPAREN params RPAREN','flow_function_call',4,'p_flow_function_call','translator.py',137),
-  ('assignment -> expression','assignment',1,'p_assignment_expression','translator.py',142),
-  ('expression -> expression PLUS term','expression',3,'p_expression_plus','translator.py',148),
-  ('expression -> expression MINUS term','expression',3,'p_expression_minus','translator.py',159),
-  ('expression -> term','expression',1,'p_expression_term','translator.py',170),
-  ('expression -> string','expression',1,'p_expression_term','translator.py',171),
-  ('string -> STRING','string',1,'p_string_def','translator.py',177),
-  ('term -> term TIMES exponent','term',3,'p_term_times','translator.py',183),
-  ('term -> term DIVIDE exponent','term',3,'p_term_divide','translator.py',193),
-  ('term -> exponent','term',1,'p_term_exponent','translator.py',202),
-  ('exponent -> factor EXP factor','exponent',3,'p_exponent_exp','translator.py',208),
-  ('exponent -> factor','exponent',1,'p_exponent_factor','translator.py',218),
-  ('exponent -> LPAREN expression RPAREN','exponent',3,'p_exponent_parent','translator.py',224),
-  ('factor -> NUMBER','factor',1,'p_factor_num','translator.py',232),
-  ('factor -> VARIABLE','factor',1,'p_factor_id','translator.py',237),
-  ('factor -> function_call','factor',1,'p_factor_function_call','translator.py',244),
-  ('function_call -> VARIABLE LPAREN RPAREN','function_call',3,'p_function_call_no_params','translator.py',251),
-  ('function_call -> VARIABLE LPAREN params RPAREN','function_call',4,'p_function_call_params','translator.py',258),
-  ('params -> params COMMA expression','params',3,'p_params','translator.py',268),
-  ('params -> expression','params',1,'p_params','translator.py',269),
+  ('assignment -> VARIABLE SETTO expression','assignment',3,'p_assignment_assign','translator3.py',132),
+  ('assignment -> VARIABLE SETTO flow','assignment',3,'p_assignment_flow','translator3.py',145),
+  ('flow -> VARIABLE CONNECT flow_functions','flow',3,'p_flow_form','translator3.py',152),
+  ('flow_functions -> flow_function_call CONNECT flow_functions','flow_functions',3,'p_flow_functions','translator3.py',158),
+  ('flow_functions -> flow_function_call','flow_functions',1,'p_flow_function','translator3.py',164),
+  ('flow_function_call -> VARIABLE LPAREN params RPAREN','flow_function_call',4,'p_flow_function_call','translator3.py',170),
+  ('assignment -> expression','assignment',1,'p_assignment_expression','translator3.py',178),
+  ('expression -> expression PLUS term','expression',3,'p_expression_plus','translator3.py',186),
+  ('expression -> expression MINUS term','expression',3,'p_expression_minus','translator3.py',198),
+  ('expression -> term','expression',1,'p_expression_term','translator3.py',210),
+  ('expression -> string','expression',1,'p_expression_term','translator3.py',211),
+  ('string -> STRING','string',1,'p_string_def','translator3.py',217),
+  ('term -> term TIMES exponent','term',3,'p_term_times','translator3.py',225),
+  ('term -> term DIVIDE exponent','term',3,'p_term_divide','translator3.py',236),
+  ('term -> exponent','term',1,'p_term_exponent','translator3.py',246),
+  ('exponent -> factor EXP factor','exponent',3,'p_exponent_exp','translator3.py',252),
+  ('exponent -> factor','exponent',1,'p_exponent_factor','translator3.py',262),
+  ('exponent -> LPAREN expression RPAREN','exponent',3,'p_exponent_parent','translator3.py',268),
+  ('factor -> NUMBER','factor',1,'p_factor_num','translator3.py',277),
+  ('factor -> VARIABLE','factor',1,'p_factor_id','translator3.py',284),
+  ('expression -> expression GT expression','expression',3,'p_expression_GT','translator3.py',292),
+  ('expression -> expression LT expression','expression',3,'p_expression_LT','translator3.py',303),
+  ('expression -> expression GE expression','expression',3,'p_expression_GE','translator3.py',314),
+  ('expression -> expression LE expression','expression',3,'p_expression_LE','translator3.py',325),
+  ('expression -> expression EQ expression','expression',3,'p_expression_EQ','translator3.py',336),
+  ('expression -> expression NE expression','expression',3,'p_expression_NE','translator3.py',347),
+  ('factor -> function_call','factor',1,'p_factor_function_call','translator3.py',363),
+  ('function_call -> VARIABLE LPAREN RPAREN','function_call',3,'p_function_call_no_params','translator3.py',370),
+  ('function_call -> VARIABLE LPAREN params RPAREN','function_call',4,'p_function_call_params','translator3.py',377),
+  ('params -> params COMMA expression','params',3,'p_params','translator3.py',386),
+  ('params -> expression','params',1,'p_params','translator3.py',387),
+  ('expression -> IF LPAREN expression RPAREN COLON expression','expression',6,'p_expression_if','translator3.py',397),
 ]
--- compiler/translator.py
+++ compiler/translator.py
@@ -5,10 +5,14 @@ from networkx.drawing.nx_pydot import graphviz_layout
 import matplotlib.pyplot as plt
 from library import *
 
+
+
+# --------------------- GRAPH VARIBLES -------------------------------
 parseGraph = None
 draw = True
 NODE_COUNTER = 0
 
+# --------------------- GRAPH FUCNTION TO ADD NODE --------------------
 def add_node(attr):
     global parseGraph
     global NODE_COUNTER
@@ -18,17 +22,15 @@ def add_node(attr):
 
     return parseGraph.nodes[NODE_COUNTER-1]
 
+# ---------------------- SYMBOL TABLE ---------------------------------
 symbol_table = dict()
-
 symbol_table["e"] = 2.718281828459045
 symbol_table["max"] = max
-
 symbol_table["load_image"] = load_image
 symbol_table["save_image"] = save_image
 symbol_table["gen_matrix"] = gen_matrix
 symbol_table["gen_vector"] = gen_vector
 symbol_table["show_image"] = show_image
-
 
 
 PLUS_OP = 1
@@ -37,6 +39,13 @@ TIMES_OP = 3
 DIVIDE_OP = 4
 
 
+# ------------------------- RESERVED WORDS ------------------------------
+reserved = {
+    'if': 'IF',
+    'else': 'ELSE',
+}
+
+# ------------------------- TOKENS --------------------------------------
 tokens = (
     'NUMBER',
     'VARIABLE',
@@ -50,9 +59,19 @@ tokens = (
     'RPAREN',
     'COMMA',
     'STRING',
-    'CONNECT'
+    'CONNECT',
+    'GT', 
+    'LT', 
+    'GE', 
+    'LE', 
+    'EQ', 
+    'NE',  
+    'IF',
+    'COLON'
 )
 
+
+# ---------------------------------- REGULAR EXPRESSIONS -----------------------
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
@@ -64,9 +83,20 @@ t_RPAREN = r'\)'
 t_COMMA = r','
 t_CONNECT = r'\->'
 
+t_GT = r'>'
+t_LT = r'<'
+t_GE = r'>='
+t_LE = r'<='
+t_EQ = r'=='
+t_NE = r'!='
+
+t_IF = r'if'
+t_COLON = r':'
+
+
+# ---------------------------------- REGEX AS FUNCTIONS ----------------------------------
 def t_NUMBER(t):
     r'\d+\.?\d*'
-
     if( t.value.find(".") > -1 ):
         t.value = float(t.value)
     else:
@@ -75,6 +105,7 @@ def t_NUMBER(t):
 
 def t_VARIABLE(t):
     r'[A-Za-z]([A-Za-z0-9_])*'
+    t.type = reserved.get(t.value, 'VARIABLE')
     return t
 
 def t_STRING(t):
@@ -83,7 +114,6 @@ def t_STRING(t):
 
     return t
 
-# ------------ boilerplate 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
@@ -92,11 +122,11 @@ t_ignore = ' \t'
 def t_error(t):
     print("Error on analysis")
     t.lexer.skip(1)
-### ---------- end 
 
-
+# -------------------------------- INITIALIZING THE LEXER ----------------------------------
 lexer = lex.lex()
 
+# -------------------------------- PARSING RULES -------------------------------------------
 def p_assignment_assign(p):
     '''
     assignment : VARIABLE SETTO expression
@@ -109,6 +139,7 @@ def p_assignment_assign(p):
     p[0] = node
 
 
+# ASSIGNMENT FLOW ----------------------------------------------------------------------
 def p_assignment_flow(p):
     '''
     assignment : VARIABLE SETTO flow
@@ -140,11 +171,16 @@ def p_flow_function_call(p):
     '''
     pass
 
+
+
+# ASSIGNMENT EXPRESSION ------------------------------------------------------------------
 def p_assignment_expression(p):
     ''' assignment : expression
     '''
     p[0] = p[1]
 
+
+# PLUS EXPRESSION -------------------------------------------------------------------------
 def p_expression_plus(p):
     """
     expression : expression PLUS term
@@ -156,6 +192,7 @@ def p_expression_plus(p):
     
     p[0] = node
 
+# MINUS EXPRESSION -------------------------------------------------------------------------
 def p_expression_minus(p):
     """
     expression : expression MINUS term
@@ -167,6 +204,7 @@ def p_expression_minus(p):
 
     p[0] = node
 
+# EXPRESSION TERM -------------------------------------------------------------------------
 def p_expression_term(p):
     """
     expression : term 
@@ -180,6 +218,8 @@ def p_string_def(p):
     '''
     p[0] =  add_node( {'type':'STRING' , 'label':f'str_{p[1]}' , 'value':p[1]} )
 
+    
+# TERM TIMES -------------------------------------------------------------------------
 def p_term_times(p):
     '''
     term : term TIMES exponent
@@ -190,6 +230,7 @@ def p_term_times(p):
     parseGraph.add_edge(node["counter"] , p[3]["counter"])
     p[0] = node
 
+# TERM DIVIDE -------------------------------------------------------------------------
 def p_term_divide(p):
     '''
     term : term DIVIDE exponent
@@ -199,6 +240,7 @@ def p_term_divide(p):
     parseGraph.add_edge(node["counter"] , p[3]["counter"])
     p[0] = node
 
+# EXPONENT -------------------------------------------------------------------------
 def p_term_exponent(p):
     '''
     term : exponent
@@ -230,17 +272,92 @@ def p_exponent_parent(p):
     parseGraph.add_edge(node["counter"] , p[2]["counter"])
     p[0] = node
 
+# FACTOR -------------------------------------------------------------------------
 def p_factor_num(p):
     ''' factor : NUMBER
     '''
     p[0] = add_node(  {'type':'NUMBER' , 'label':f'NUM_{p[1]}' , 'value':p[1]} )
 
+
+# VARIABLE -------------------------------------------------------------------------
 def p_factor_id(p):
     ''' factor : VARIABLE
     '''
     p[0] = add_node(  {'type':'VARIABLE' , 'label':f'VAR_{p[1]}' , 'value':p[1]} )
-    
 
+
+# COMPARISON OPERATORS -------------------------------------------------------------------------
+def p_expression_GT(p):
+    """
+    expression : expression GT expression
+    """
+
+    node = add_node( {'type':'GT' , 'label':'>' , 'value':''} )
+    parseGraph.add_edge(node["counter"] , p[1]["counter"])
+    parseGraph.add_edge(node["counter"] , p[3]["counter"])
+
+    p[0] = node
+
+def p_expression_LT(p):
+    """
+    expression : expression LT expression
+    """
+
+    node = add_node( {'type':'LT' , 'label':'<' , 'value':''} )
+    parseGraph.add_edge(node["counter"] , p[1]["counter"])
+    parseGraph.add_edge(node["counter"] , p[3]["counter"])
+
+    p[0] = node
+
+def p_expression_GE(p):
+    """
+    expression : expression GE expression
+    """
+
+    node = add_node( {'type':'GE' , 'label':'>=' , 'value':''} )
+    parseGraph.add_edge(node["counter"] , p[1]["counter"])
+    parseGraph.add_edge(node["counter"] , p[3]["counter"])
+
+    p[0] = node
+
+def p_expression_LE(p):
+    """
+    expression : expression LE expression
+    """
+
+    node = add_node( {'type':'LE' , 'label':'<=' , 'value':''} )
+    parseGraph.add_edge(node["counter"] , p[1]["counter"])
+    parseGraph.add_edge(node["counter"] , p[3]["counter"])
+
+    p[0] = node
+
+def p_expression_EQ(p):
+    """
+    expression : expression EQ expression
+    """
+
+    node = add_node( {'type':'EQ' , 'label':'==' , 'value':''} )
+    parseGraph.add_edge(node["counter"] , p[1]["counter"])
+    parseGraph.add_edge(node["counter"] , p[3]["counter"])
+
+    p[0] = node
+
+def p_expression_NE(p):
+    """
+    expression : expression NE expression
+    """
+
+    node = add_node( {'type':'NE' , 'label':'!=' , 'value':''} )
+    parseGraph.add_edge(node["counter"] , p[1]["counter"])
+    parseGraph.add_edge(node["counter"] , p[3]["counter"])
+
+    p[0] = node
+
+
+# LOGICAL OPERATORS -------------------------------------------------------------------------
+#PENDING
+
+# FUNCTION CALL -----------------------------------------------------------------------------------------
 def p_factor_function_call(p):
     '''
     factor : function_call
@@ -259,7 +376,6 @@ def p_function_call_params(p):
     '''
     function_call : VARIABLE LPAREN params RPAREN
     '''
-    #print(p[3])
     node = add_node(  {'type':'FUNCTION_CALL' , 'label':f'FUN_{p[1]}' , 'value':p[1]} )
     for n in p[3]:
         parseGraph.add_edge(node["counter"] , n["counter"])
@@ -275,17 +391,32 @@ def p_params(p):
     else:
         p[0] = [p[1]]
 
+
+# CONDITIONAL STATEMENTS --------------------------------------------------------------------
+def p_expression_if(p):
+    'expression : IF LPAREN expression RPAREN COLON expression'
+    node = add_node({'type': 'IF', 'label': 'IF', 'value': ''})
+    condition_node = p[3]
+    expression_node = p[6]
+
+    parseGraph.add_edge(node["counter"], condition_node["counter"], label='condition')
+    parseGraph.add_edge(node["counter"], expression_node["counter"], label='expression')
+
+    p[0] = node
+
+# BOILER PLATE ------------------------------------------------------------------------------
 def p_error(p):
     print("Syntax error on input ", p)
 
-
+# ------------------------------------- PARSE TREE ------------------------------------------
 def execute_parse_tree(tree):
     root = tree.nodes[0]
     root_id = 0
     res = visit_node(tree, root_id, -1)
-    if( type(res) == int or type(res) == float ):
+    if( type(res) == int or type(res) == float or type(res) == bool):
         print("TREE_RESULT: " , res)
 
+# --------------------------------------- FUNCTION TO VISIT NODES -----------------------------
 def visit_node(tree, node_id, from_id):
     children = tree.neighbors(node_id)
 
@@ -295,47 +426,68 @@ def visit_node(tree, node_id, from_id):
             res.append(visit_node(tree, c, node_id) )
 
     current_node = tree.nodes[node_id]
-    # print(f"From Node {node_id}" , res)
 
+    # Initial node logic
     if( current_node["type"] == "INITIAL" ):
         return res[0]
-
-
+    
+    # Assign node logic 
     if( current_node["type"] == "ASSIGN" ):
-        
         symbol_table[ res[0] ] = res[1]
         return res[1]
-
     if( current_node["type"] == "VARIABLE_ASSIGN" ):
         return current_node["value"]
-
-
+    
+    # Number node logic
     if( current_node["type"] == "NUMBER" ):
         return current_node["value"]
     
+    #String node logic
     if( current_node["type"] == "STRING" ):
         return current_node["value"]
     
-    if( current_node["type"] == "PLUS" ):
-        return res[0] + res[1]
-    
+    #Variable node logic
     if( current_node["type"] == "VARIABLE" ):
         return symbol_table[current_node["value"]]
     
+    #Arithmetic operations node logic
+    if( current_node["type"] == "PLUS" ):
+        return res[0] + res[1]
     if( current_node["type"] == "MINUS" ):
         return res[0] - res[1]
-
-    if( current_node["type"] == "POWER" ):
-        return pow(res[0], res[1])
-    
-    if(current_node["type"] == "GROUP"):
-        return res[0]
-
     if( current_node["type"] == 'TIMES'):
         return res[0] * res[1]
     if( current_node["type"] == 'DIVIDE'):
         return res[0] / res[1]
+    if( current_node["type"] == "POWER" ):
+        return pow(res[0], res[1])
+    if(current_node["type"] == "GROUP"):
+        return res[0]
     
+
+    #Comparison Operators node logic
+    if current_node["type"] == "GT":
+        return res[0] > res[1]
+    if current_node["type"] == "LT":
+        return res[0] < res[1]
+    if current_node["type"] == "GE":
+        return res[0] >= res[1]
+    if current_node["type"] == "LE":
+        return res[0] <= res[1]
+    if current_node["type"] == "EQ":
+        return res[0] == res[1]
+    if current_node["type"] == "NE":
+        return res[0] != res[1]
+    
+
+    #Simple IF statement node logic
+    if current_node['type'] == 'IF':
+        if res[0]:
+            return res[1]
+        else:
+            return None  
+
+    #Function call node logic
     if( current_node["type"] == "FUNCTION_CALL"):
         v = current_node["value"]
         if v in symbol_table:
@@ -368,9 +520,11 @@ def visit_node(tree, node_id, from_id):
                 print(f"Error {v} IS NOT on symbol table ")
                 return "Error"
 
+# ---------------------------------------- BUILDING THE PARSER ------------------------------------
 parser = yacc.yacc()
-while True:
 
+# ---------------------------------------- LEXER EXECUTION  -------------------------------
+while True:
     try:
         data = input(">")
         if(data == 'exit'):
